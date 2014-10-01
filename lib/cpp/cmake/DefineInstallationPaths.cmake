d3142b9aa9e110a56a0d0ebc7c5e1b15aad693a4
Pascal Bach <pascal.bach@siemens.com>
2014-10-01 17:59:58 +0200

    CMake: Make install path configurable

---
+++ lib/cpp/cmake/DefineInstallationPaths.cmake
@@ -0,0 +1,5 @@
+# Define the default install paths
+set(BIN_INSTALL_DIR "bin" CACHE PATH "The binary install dir (default: bin)")
+set(LIB_INSTALL_DIR "lib${LIB_SUFFIX}" CACHE PATH "The library install dir (default: lib${LIB_SUFFIX})")
+set(INCLUDE_INSTALL_DIR "include" CACHE PATH "The library install dir (default: include)")
+set(CMAKE_INSTALL_DIR "cmake" CACHE PATH "The subdirectory to install cmake config files (default: cmake)")
