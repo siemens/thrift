ba9e4fcdb6b03f0caf855b0ce2df28895c32af94
Pascal Bach <pascal.bach@siemens.com>
2014-11-11 17:49:32 +0100

    Do more auto detection for configure options.

--- cmake/DefineOptions.cmake
+++ cmake/DefineOptions.cmake
@@ -1,26 +1,46 @@
-# Libraries to build
-option(WITH_CPP "Build the C++ library" ON)
-option(WITH_C_GLIB "Build the GLib C library" ON)
-option(WITH_JAVA "Build with Java library" ON)
+include(CMakeDependentOption)
 
-
-# C++ library options
-option(WITH_ZLIB "Build with ZLIB support" OFF)
-option(WITH_LIBEVENT "Build with libevent support" OFF)
-option(WITH_QT4 "Build with Qt4 support" OFF)
-option(WITH_OPENSSL "Build with OpenSSL support" ON)
-
-option(WITH_BOOSTTHREADS "Build with Boost thread support" OFF)
-option(WITH_STDTHREADS "Build with C++ std::thread support" OFF)
-
-option(WITH_SHARED_LIB "Build with a shared library" ON)
-option(WITH_STATIC_LIB "Build with a static library" ON)
-
+# Additional components
 option(BUILD_COMPILER "Build the thrift compiler" ON)
 option(BUILD_TESTING "Build with unit tests" ON)
 option(BUILD_EXAMPLES "Build examples" ON)
+option(BUILD_LIBRARIES "Build the thrfit libraries" ON)
 
-#NOTE: C++ compiler options are defined in the compile CMakeLists.txt
+# Libraries to build
+
+# C++
+find_package(Boost 1.53 QUIET)
+CMAKE_DEPENDENT_OPTION(WITH_CPP "Build the C++ library" ON
+                       "BUILD_LIBRARIES;Boost_FOUND" OFF)
+find_package(ZLIB QUIET)
+CMAKE_DEPENDENT_OPTION(WITH_ZLIB "Build with ZLIB support" ON
+                       "ZLIB_FOUND" OFF)
+find_package(Libevent QUIET)
+CMAKE_DEPENDENT_OPTION(WITH_LIBEVENT "Build with libevent support" ON
+                       "Libevent_FOUND" OFF)
+find_package(Qt4 QUIET)
+CMAKE_DEPENDENT_OPTION(WITH_QT4 "Build with Qt4 support" ON
+                       "Qt4_FOUND" OFF)
+find_package(OpenSSL QUIET)
+CMAKE_DEPENDENT_OPTION(WITH_OPENSSL "Build with OpenSSL support" ON
+                       "OpenSSL_FOUND" OFF)
+option(WITH_BOOSTTHREADS "Build with Boost thread support" OFF)
+option(WITH_STDTHREADS "Build with C++ std::thread support" OFF)
+
+# C GLib
+find_package(GTK2 QUIET)
+CMAKE_DEPENDENT_OPTION(WITH_C_GLIB "Build the GLib C library" ON
+                       "BUILD_LIBRARIES;GTK2_FOUND" OFF)
+# Java
+find_package(Java QUIET)
+CMAKE_DEPENDENT_OPTION(WITH_JAVA "Build with Java library" ON
+                       "BUILD_LIBRARIES;JAVA_FOUND" OFF)
+
+# Common library options
+option(WITH_SHARED_LIB "Build with a shared library" ON)
+option(WITH_STATIC_LIB "Build with a static library" ON)
+
+#NOTE: C++ compiler options are defined in the lib/cpp/CMakeLists.txt
 
 
 # Visual Studio only options
