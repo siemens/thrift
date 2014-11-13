7b742ef657513ee9276693559d4c5e6f30d4cd2a
Pascal Bach <pascal.bach@siemens.com>
2014-11-13 14:15:10 +0100

    Put build artifacts in bin and lib folder

--- cmake/DefineCMakeDefaults.cmake
+++ cmake/DefineCMakeDefaults.cmake
@@ -28,3 +28,11 @@ endif (NOT CMAKE_BUILD_TYPE)
 
 # Create the compile command database for clang by default
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+
+# Put the libaries and binaries that get built into directories at the
+# top of the build tree rather than in hard-to-find leaf
+# directories. This simplifies manual testing and the use of the build
+# tree rather than installed thrift libraries.
+set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib)
+set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib)
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
