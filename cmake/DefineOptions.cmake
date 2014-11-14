0984a1459a5e3032f985870ab967dcbe31ea6edb
Pascal Bach <pascal.bach@siemens.com>
2014-11-14 15:20:55 +0100

    Restructure CMake configuration
    - Add configuration summary
    - Add platform specific options file

--- cmake/DefineOptions.cmake
+++ cmake/DefineOptions.cmake
@@ -1,17 +1,26 @@
 include(CMakeDependentOption)
 
 # Additional components
-option(BUILD_COMPILER "Build the thrift compiler" ON)
+option(BUILD_COMPILER "Build Thrift compiler" ON)
 option(BUILD_TESTING "Build with unit tests" ON)
 option(BUILD_EXAMPLES "Build examples" ON)
-option(BUILD_LIBRARIES "Build the thrfit libraries" ON)
+option(BUILD_LIBRARIES "Build Thrfit libraries" ON)
 
 # Libraries to build
 
+# Each language library can be enabled or disabled using the WITH_<LANG> flag.
+# By default CMake checks if the required dependencies for a language are present
+# and enables the library if all are found. This means the default is to build as
+# much as possible but leaving out libraries if their dependencies are not met.
+
 # C++
 find_package(Boost 1.53 QUIET)
-CMAKE_DEPENDENT_OPTION(WITH_CPP "Build the C++ library" ON
+CMAKE_DEPENDENT_OPTION(WITH_CPP "Build C++ library" ON
                        "BUILD_LIBRARIES;Boost_FOUND" OFF)
+# NOTE: Currently the following options are C++ specific,
+# but in future other libraries might reuse them. 
+# So they are not dependent on WIHT_CPP but setting them without WITH_CPP currently
+# has no effect.
 find_package(ZLIB QUIET)
 CMAKE_DEPENDENT_OPTION(WITH_ZLIB "Build with ZLIB support" ON
                        "ZLIB_FOUND" OFF)
@@ -29,43 +38,45 @@ option(WITH_STDTHREADS "Build with C++ std::thread support" OFF)
 
 # C GLib
 find_package(GTK2 QUIET)
-CMAKE_DEPENDENT_OPTION(WITH_C_GLIB "Build the GLib C library" ON
+CMAKE_DEPENDENT_OPTION(WITH_C_GLIB "Build C (GLib) library" ON
                        "BUILD_LIBRARIES;GTK2_FOUND" OFF)
 # Java
 find_package(Java QUIET)
-CMAKE_DEPENDENT_OPTION(WITH_JAVA "Build with Java library" ON
+CMAKE_DEPENDENT_OPTION(WITH_JAVA "Build Java library" ON
                        "BUILD_LIBRARIES;JAVA_FOUND" OFF)
 
 # Common library options
-option(WITH_SHARED_LIB "Build with a shared library" ON)
-option(WITH_STATIC_LIB "Build with a static library" ON)
+option(WITH_SHARED_LIB "Build shared libraries" ON)
+option(WITH_STATIC_LIB "Build static libraries" ON)
 
 #NOTE: C++ compiler options are defined in the lib/cpp/CMakeLists.txt
 
 # Visual Studio only options
 if(MSVC)
 option(WITH_MT "Build unsing MT instead of MT (MSVC only)" OFF)
-
-# For Debug build types, append a "d" to the library names.
-set(CMAKE_DEBUG_POSTFIX "d" CACHE STRING "Set debug library postfix" FORCE)
-set(CMAKE_RELEASE_POSTFIX "" CACHE STRING "Set release library postfix" FORCE)
-
-# Replace MD with MT
-if(WITH_MT)
-    set(CompilerFlags
-            CMAKE_CXX_FLAGS
-            CMAKE_CXX_FLAGS_DEBUG
-            CMAKE_CXX_FLAGS_RELEASE
-            CMAKE_C_FLAGS
-            CMAKE_C_FLAGS_DEBUG
-            CMAKE_C_FLAGS_RELEASE
-            )
-    foreach(CompilerFlag ${CompilerFlags})
-      string(REPLACE "/MD" "/MT" ${CompilerFlag} "${${CompilerFlag}}")
-    endforeach()
-    set(STATIC_POSTFIX "mt" CACHE STRING "Set static library postfix" FORCE)
-else(WITH_MT)
-    set(STATIC_POSTFIX "md" CACHE STRING "Set static library postfix" FORCE)
-endif(WITH_MT)
-
 endif(MSVC)
+
+macro(PRINT_CONFIG_SUMMARY)
+message(STATUS "----------------------------------------------------------")
+message(STATUS "Thrift version: ${thrift_VERSION} (${thrift_VERSION_MAJOR}.${thrift_VERSION_MINOR}.${thrift_VERSION_PATCH})")
+message(STATUS "Thrift package version: ${PACKAGE_VERSION}")
+message(STATUS "Build configuration Summary")
+message(STATUS "  Build Thrift compiler: ${BUILD_COMPILER}")
+message(STATUS "  Build with unit tests: ${BUILD_TESTING}")
+message(STATUS "  Build examples: ${BUILD_EXAMPLES}")
+message(STATUS "  Build Thrfit libraries: ${BUILD_LIBRARIES}")
+message(STATUS " Language libraries:")
+message(STATUS "  Build C++ library: ${WITH_CPP}")
+message(STATUS "  Build C (GLib) library: ${WITH_C_GLIB}")
+message(STATUS "  Build Java library: ${WITH_JAVA}")
+message(STATUS " Library features:")
+message(STATUS "  Build shared libraries: ${WITH_SHARED_LIB}")
+message(STATUS "  Build static libraries: ${WITH_STATIC_LIB}")
+message(STATUS "  Build with ZLIB support: ${WITH_ZLIB}")
+message(STATUS "  Build with libevent support: ${WITH_LIBEVENT}")
+message(STATUS "  Build with Qt4 support: ${WITH_QT4}")
+message(STATUS "  Build with OpenSSL support: ${WITH_OPENSSL}")
+message(STATUS "  Build with Boost thread support: ${WITH_BOOSTTHREADS}")
+message(STATUS "  Build with C++ std::thread support: ${WITH_STDTHREADS}")
+message(STATUS "----------------------------------------------------------")
+endmacro(PRINT_CONFIG_SUMMARY)
